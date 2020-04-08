513369ba7e99db288a61dbc02758292191607e4f
Aitor <aitor.ballesteros.pavon@gmail.com>
2020-04-08 11:45:21 +0200

    Corrections in market

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -13,32 +13,52 @@ class MarketController extends Controller
 {
 
     public function updateMarket()
-    {
+    {  
         $response = array('code' => 400, 'error_msg' => []);
         $groups = Group::all()
         ->map(function ($group) {
 
             $salesByGroup = DB::table('sales')->where('id_group', '=', $group->id)->get();
             $karatekas = Karateka::all();
+          
+
             foreach ($salesByGroup  as $value) {
               $karatekas = $karatekas->where('id', '<>', $value->id_karatekas); // Get all the karateka that don't have that id 
             }
 
-            $karatekaRandom = $karatekas->random(5);   
-            foreach($karatekaRandom as $karateka){
+            $numberKaratekasInMarket = 5;
+            $karatekaRandom = $karatekas->random($numberKaratekasInMarket);  
+    
+    
+          
+            if( count($karatekas) <= $numberKaratekasInMarket){
+                $re = "All karatekas are in group";
+                var_dump($re);
+              
+              $response = array('code' => 200, 'msg' => 'All karatekas are in group',);
 
-                $onSalePlayer = new Market();
-              
-                $onSalePlayer->id_group = $group->id;
-              
-                $onSalePlayer->id_karatekas = $karateka->id;
-                $onSalePlayer->date_release = $karateka->created_at;
-                $onSalePlayer->save();      
-            }  
-            
-            $response = array('code' => 200, 'group' => $onSalePlayer, 'msg' => 'Karateka on sale created');
+            }else{
+
+                $er = "Goog, there are karatekas available in market";
+                var_dump($er);
+       
+                foreach($karatekaRandom as $karateka){
+    
+                    $onSalePlayer = new Market();
+                  
+                    $onSalePlayer->id_group = $group->id;
+                  
+                    $onSalePlayer->id_karatekas = $karateka->id;
+                    $onSalePlayer->date_release = $karateka->created_at;
+                    $onSalePlayer->save();      
+                }  
+                
+               $response = array('code' => 200, 'group' => $onSalePlayer, 'msg' => 'Karateka on sale created');
+            } 
+
+  
         });
-     
+        return response($response, $response['code']);
     }
 
     public function showMarketByGroup($idGroup)
@@ -51,7 +71,7 @@ class MarketController extends Controller
                 $karatekasInMarket = $karateka->karatekasByGroupInMarket;
                 return   $response = array('code' => 200, 'Karatekas by group in market' => $karatekasInMarket, 'msg' => 'Get all karatekas by group in market');
             } else {
-                $response = array('code' => 401, 'error_msg' => 'Unautorized');
+                return $response = array('code' => 401, 'error_msg' => 'Unautorized');
             }
         } catch (\Exception $exception) {
             $response = array('code' => 500, 'error_msg' => $exception->getMessage());
