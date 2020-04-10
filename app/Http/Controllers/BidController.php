8d1284db4c45d2bea28882380aa9347c64964964
Aitor <aitor.ballesteros.pavon@gmail.com>
2020-04-10 16:34:08 +0200

    Fixes response in AddParticipant

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -13,82 +13,75 @@ class BidController extends Controller
 {
     public function createBid(Request $request, $idGroup){
 
-        $response = array('code' => 400, 'error_msg' => []);
-        
-        
-        if(isset($request) && isset($idGroup)){
-        
-           
+            $response = array('code' => 400, 'error_msg' => []);
+
             if (!$request->id_karateka) array_push($response['error_msg'], 'id_karateka is required');
             if (!$idGroup) array_push($response['error_msg'], 'idGroup is required');
+            if (!$request->bid) array_push($response['error_msg'], 'bid is required');
+
             try {
                 
                 $karateka = Karateka::find($idGroup);
                 
                 if (!empty($karateka)) {
                     
-                    $response = ['karatekas' => $karateka->id, 'groups' => []];
+                   // $response = ['karatekas' => $karateka->id, 'groups' => []];
                     $karatekasInMarket = $karateka->karatekasByGroupInMarket;
-
-                    $filterKarateka = $karatekasInMarket ->filter(function ($karateka) use ($request) {
+               
+                    $filterKarateka = $karatekasInMarket ->filter(function ($karateka) use ($request, & $response) {
                        // Si id es igual al id de la request, cogemos ese karateka.
-                      
+                
                    //  return $karateka->id == $request->id_karateka;
-                 
-                   var_dump($karateka->id);
-                   var_dump($request->id_karateka);
                             if($karateka->id == $request->id_karateka){
-
+                         
                                 $market = Market::all()
-                                ->map(function ($market) use ($karateka, $request){
+                                ->map(function ($market) use ($karateka, $request, & $response){ // Pasamos el response por referencia con &
                                     if($market->id_group == $karateka->pivot->id_group && $market->id_karatekas == $karateka->id){
-                                       
+                                     
+                                        $bid = new Bid();
                                         $participant = Participant::all()
-                                        ->map(function ($participant) use ($karateka, $request, $market){
+                                      
+                                        ->map(function ($participant) use ($karateka, $request, $market, & $response, & $bid){ // Pasamos el response y bid como referencia con &
 
                                             if($participant->id == $request->id_participant && $karateka->pivot->id_group == $participant->id_group){
-
-                                                $bid = new Bid();
+                                                
                                                 $bid->id_market = $market->id;
                                                 $bid->id_group = $market->id_group;
                                                 $bid->id_karatekas = $market->id_karatekas;
                                                // $bid->bid = $request->bid;
                                                 $bid->id_participants = $request->id_participant;
                                                 self::restrictionMinimunBid($request, $karateka, $bid);
-                                               
-                                             //   
-                                     
-                                               
+                                            
+                                                // var_dump($response);
+                                                // die;
+                                                $response = array('code' => 200, 'Bid' => $bid, 'msg' => 'Bid created'); ///ESTE ES EL RESPONSE QUE NO FUNCIONA 
                                             }else{
                                                 $error ="The participant isnt in this group, it is not correct";
                                                 var_dump($error);
-                                                $response = array('code' => 401, 'error_msg' => 'The participant isnt in this group, it is not correct');
+                                               // $response = array('code' => 401, 'error_msg' => 'The participant isnt in this group, it is not correct');
                                             }
                                         });
 
-                                        $response = array('code' => 200, 'Bid' => $bid, 'msg' => 'Bid created'); ///ESTE ES EL RESPONSE QUE NO FUNCIONA
                                     }
+                                  
+
                                 });
-                                
+                               
                             }else{
                                 $error ="Karateka isnt in this market, it is not correct";
-                                                var_dump($error);
-                                $response = array('code' => 401, 'error_msg' => 'Karateka isnt in this market, it is not correct');
+                                var_dump($error);
+                               // $response = array('code' => 401, 'error_msg' => 'Karateka isnt in this market, it is not correct');
                             }   
                     });
-                    
                 } else {
                     $response = array('code' => 401, 'error_msg' => 'Unautorized');
                 }
             } catch (\Exception $exception) {
                 $response = array('code' => 500, 'error_msg' => $exception->getMessage());
             }
-        }
-    
-        return response($response, $response['code']);
+       return response($response, $response['code']);
 
     }
-
     public function restrictionMinimunBid(Request $request, $karateka, $bid)
     {
         $allKaratekas = Karateka::all()
@@ -102,6 +95,7 @@ class BidController extends Controller
                     $bid->save();
                     $msg ="The bid is more than the value of karateka /  Bid created.";
                     var_dump($msg);
+  
                    }
                    else{
                        $error ="The bid is less than the value of karateka";
@@ -114,5 +108,4 @@ class BidController extends Controller
   
     }
 
-   
 }
